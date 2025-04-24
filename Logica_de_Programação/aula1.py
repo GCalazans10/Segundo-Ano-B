ff99f1f51ed0ec9dcaf9aa1441a38ac566ac8a6c
GCalazans10 <g.calazans10@gmail.com>
2025-04-24 13:20:39 -0300

    Material de Apoio

---
+++ Logica_de_Programação/aula1.py
@@ -0,0 +1,4 @@
+# Na programação, as variáveis desempenham um papel fundamental como base para o desenvolvimento de algoritmos e a aplicação da lógica de programação.
+# Elas são elementos que permitem armazenar e manipular valores, representando informações temporárias que serão utilizadas durante a execução de um programa.
+# Exploraremos o conceito de variáveis, os diferentes tipos existentes, sua declaração e atribuição, além de sua importância na construção de algoritmos e na lógica de programação.
+

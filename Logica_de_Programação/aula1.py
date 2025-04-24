180ebd6b1c5f6c4e072c3ae4cfb427cb495978b8
GCalazans10 <g.calazans10@gmail.com>
2025-04-24 13:24:29 -0300

    primeiro_commit

--- Logica_de_Programação/aula1.py
+++ Logica_de_Programação/aula1.py
@@ -2,3 +2,4 @@
 # Elas são elementos que permitem armazenar e manipular valores, representando informações temporárias que serão utilizadas durante a execução de um programa.
 # Exploraremos o conceito de variáveis, os diferentes tipos existentes, sua declaração e atribuição, além de sua importância na construção de algoritmos e na lógica de programação.
 
+#
